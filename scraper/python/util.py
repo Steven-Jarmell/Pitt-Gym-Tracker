ba54624256c58c7ddc8feec87e1ebeba38965966
Steven Jarmell <sjj27@pitt.edu>
2024-02-12 15:25:37 -0500

    Restructure files and finish scraper

--- scraper/python/util.py
+++ scraper/python/util.py
@@ -3,16 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_connection_string() -> str:
     (host_name, username, password, db_name) = get_db_env_variables()
 
     return f'mysql+mysqlconnector://{username}:{password}@{host_name}:3306/{db_name}'
 
+
 def get_db_env_variables() -> tuple[str, str, str, str]:
-    host_name= os.getenv("DB_HOST")
-    username= os.getenv("DB_USERNAME")
-    password= os.getenv("DB_PASSWORD")
-    db_name= os.getenv("DB_NAME")
+    host_name = os.getenv("DB_HOST")
+    username = os.getenv("DB_USERNAME")
+    password = os.getenv("DB_PASSWORD")
+    db_name = os.getenv("DB_NAME")
 
     if host_name == None or username == None or password == None or db_name == None:
         print("Environment Variables Not Set Correctly:")
@@ -24,10 +26,11 @@ def get_db_env_variables() -> tuple[str, str, str, str]:
             print(" - DATABASE_PASSWORD Variable Not Set")
         if not db_name:
             print(" - DATABASE Variable Not Set")
-        
+
         exit(1)
 
     return (host_name, username, password, db_name)
+
 
 def get_page_url() -> str:
     url = os.getenv("CONNECT_2_URL")
@@ -35,7 +38,7 @@ def get_page_url() -> str:
     if url == None:
         print("Environment Variables Not Set Correctly:")
         print(" - CONNECT_2_URL Variable Not Set")
-        
+
         exit(1)
-    
+
     return url
