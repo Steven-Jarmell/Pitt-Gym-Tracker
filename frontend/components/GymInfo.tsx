b55231b66b6875dd8e353b039ec6cb4033226f32
Steven Jarmell <sjj27@pitt.edu>
2024-02-13 16:44:50 -0500

    Fetch data from DB and naiively display

---
+++ frontend/components/GymInfo.tsx
@@ -0,0 +1,19 @@
+type GymInfoProps = {
+    name: string,
+    status: boolean,
+    count: number,
+    lastUpdated: Date,
+}
+
+const GymInfo = ({name, status, count, lastUpdated}: GymInfoProps) => {
+    return (
+        <div>
+            <p>Name: {name}</p>
+            <p>Status: {status ? "Open" : "Closed"}</p>
+            <p>Count: {count}</p>
+            <p>Last Updated: {lastUpdated.toUTCString()}</p>
+        </div>
+    )
+}
+
+export default GymInfo
