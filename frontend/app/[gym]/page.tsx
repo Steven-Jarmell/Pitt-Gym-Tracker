5941de202e40a5262a19df02bfbbd481c57787bb
Steven Jarmell <sjj27@pitt.edu>
2024-02-21 10:21:08 -0500

    Use routes to dynamically fetch gym data

--- frontend/app/[gym]/page.tsx
+++ frontend/app/[gym]/page.tsx
@@ -1,5 +1,23 @@
+'use client'
+
+import GymGraph from "@/components/GymGraph/GymGraph";
+import { getAllGymNames } from "@/util/api";
+import { useEffect, useState } from "react";
+
 export default function Gym({params} : {params: {gym: string}}) {
+    const gymCategory = params.gym.replaceAll('_', ' ');
+    const [gymNames, setGymNames] = useState<string[]>([]);
+
+    useEffect(() => {
+        getAllGymNames().then(names => {
+            let filteredNames = names.filter(name => name.name.includes(gymCategory));
+            setGymNames(filteredNames.map(curName => curName.name))
+    })
+
+    })
     return (
-        <p>{params.gym}</p>
+        <div>
+            {gymNames.map((name, i) => <GymGraph key={i} gymName={name} />)}
+        </div>
     );
 }
