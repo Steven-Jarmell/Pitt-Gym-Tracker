5802027c78af5ef4e44a45709a07137c87eb2ab9
Steven Jarmell <sjj27@pitt.edu>
2024-03-19 14:36:58 -0400

    Hard code port bc it doesnt matter

--- scraper/python/util.py
+++ scraper/python/util.py
@@ -5,18 +5,17 @@ load_dotenv()
 
 
 def get_connection_string() -> str:
-    (host_name, username, password, db_port) = get_db_env_variables()
+    (host_name, username, password) = get_db_env_variables()
 
-    return f'postgresql://{username}:{password}@{host_name}:{db_port}/postgres'
+    return f'postgresql://{username}:{password}@{host_name}:6543/postgres'
 
 
 def get_db_env_variables() -> tuple[str, str, str, str]:
     host_name = os.getenv("DB_HOST")
     username = os.getenv("DB_USERNAME")
     password = os.getenv("DB_PASSWORD")
-    db_port = os.getenv("DB_PORT")
 
-    if host_name == None or username == None or password == None or db_port == None:
+    if host_name == None or username == None or password == None:
         print("Environment Variables Not Set Correctly:")
         if not host_name:
             print(" - DATABASE_HOST Variable Not Set")
@@ -24,9 +23,7 @@ def get_db_env_variables() -> tuple[str, str, str, str]:
             print(" - DATABASE_USERNAME Variable Not Set")
         if not password:
             print(" - DATABASE_PASSWORD Variable Not Set")
-        if not db_port:
-            print(" - DB_PORT Variable Not Set")
 
         exit(1)
 
-    return (host_name, username, password, db_port)
+    return (host_name, username, password)
