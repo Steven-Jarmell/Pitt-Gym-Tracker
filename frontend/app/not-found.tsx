910be2462103052b15fa8b995bd2191e19a5b1fb
Steven Jarmell <sjj27@pitt.edu>
2024-10-13 10:38:28 -0700

    Update 404 page, as Pitt's QB is now good

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -3,15 +3,12 @@ import RocPic from "@/images/roc.png"
 
 export default function Custom404() {
   return (
-    <div className="flex flex-col justify-center items-center bg-black h-[100vh] w-[100vw]">
+    <div className="flex flex-col justify-center items-center bg-[#040D12] h-[100vh] w-[100vw]">
       <div className="mx-auto max-w-7xl px-6 text-center lg:px-8">
         <p className="text-base font-semibold leading-8 text-white">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
           Page not found
         </h1>
-        <p className="mt-4 text-base text-white/70 sm:mt-6">
-          Much like our quarterback, this page does not exist
-        </p>
         <div className="mt-10 flex justify-center">
           <a href="/" className="text-sm font-semibold leading-7 text-white">
             <span aria-hidden="true">&larr;</span> Back to home
